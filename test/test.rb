afe122104deb0c39fe24b203b3db0b9ca6bbbbbc
Michelle9 <jhoanmary@gmail.com>
2014-09-29 19:57:27 +0100

    Mostrar pagina principal

---
+++ test/test.rb
@@ -0,0 +1,20 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'minitest/autorun'
+require 'rack/test'
+require_relative '../twitter.rb'
+
+include Rack::Test::Methods
+	
+	def app
+		Sinatra::Application
+	end
+	
+describe "SYTW P2" do
+
+  it "Mostrar pagina principal"
+	get '/'
+	assert last_response.ok?
+  end
+  
+end
