630a4353a9f918ee471a3a3117ec658cc2b783b3
Michelle9 <jhoanmary@gmail.com>
2014-09-30 23:24:39 +0100

    Borrando el configure.rb

--- configure.rb
+++
@@ -1,8 +0,0 @@
-def my_twitter_client
-  Twitter::REST::Client.new do |config|
-    config.consumer_key       = '2TAJjO9OArofGYXdE1RyC3AUc'
-    config.consumer_secret    = 'HCMIaDmN7a7j1AhPGSyViD9wHNvSmlc1SVWMpG1PX6V8Yewsc5'
-    config.access_token        = '240486908-zuGvx9whxw5Gu6Url0V0zmHJrVznbk6KUvHbyqlQ'
-    config.access_token_secret = 'vDfBbc0dljrs3OjXOYwxzWmCfpuDpgOAvHiogU5url9E8'
-  end
-end
